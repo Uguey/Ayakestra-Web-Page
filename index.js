d41f06277753e0b3c3aea75053f821d591e64533
Uguey <fabiorichandfamous@gmail.com>
2016-05-27 18:47:56 +0200

    Page added

---
+++ index.js
@@ -0,0 +1,275 @@
+$(document).ready(function(){
+
+	console.log(window.innerWidth);
+
+	/**** Games ***/
+	// First row
+	// First game
+	$("#source1").mouseenter(function(){
+		$("#source1").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source1").mouseleave(function(){
+		$("#source1").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+
+	// Second game
+	$("#windows2").mouseenter(function(){
+		$("#windows2").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows2").mouseleave(function(){
+		$("#windows2").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#android2").mouseenter(function(){
+		$("#android2").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#android2").mouseleave(function(){
+		$("#android2").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source2").mouseenter(function(){
+		$("#source2").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source2").mouseleave(function(){
+		$("#source2").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+
+	// Third game
+	$("#windows3").mouseenter(function(){
+		$("#windows3").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows3").mouseleave(function(){
+		$("#windows3").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source3").mouseenter(function(){
+		$("#source3").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source3").mouseleave(function(){
+		$("#source3").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+
+	// Second row
+	// Fourth game
+	$("#windows4").mouseenter(function(){
+		$("#windows4").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows4").mouseleave(function(){
+		$("#windows4").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source4").mouseenter(function(){
+		$("#source4").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source4").mouseleave(function(){
+		$("#source4").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+
+	// Fifth game
+	$("#windows5").mouseenter(function(){
+		$("#windows5").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows5").mouseleave(function(){
+		$("#windows5").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#android5").mouseenter(function(){
+		$("#android5").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#android5").mouseleave(function(){
+		$("#android5").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source5").mouseenter(function(){
+		$("#source5").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source5").mouseleave(function(){
+		$("#source5").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	// Sixth game
+	$("#windows6").mouseenter(function(){
+		$("#windows6").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows6").mouseleave(function(){
+		$("#windows6").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source6").mouseenter(function(){
+		$("#source6").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source6").mouseleave(function(){
+		$("#source6").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	// Seventh game
+	$("#windows7").mouseenter(function(){
+		$("#windows7").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#windows7").mouseleave(function(){
+		$("#windows7").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#android7").mouseenter(function(){
+		$("#android7").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#android7").mouseleave(function(){
+		$("#android7").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	$("#source7").mouseenter(function(){
+		$("#source7").animate({
+			fontSize: '1.1em',
+			opacity: '0.5'
+		},"fast");	
+	});
+
+	$("#source7").mouseleave(function(){
+		$("#source7").animate({
+			fontSize: '1em',
+			opacity: '1'
+		},"fast");	
+	});
+
+	/**** Images ***/
+	$("#containerThreeTwitter").mouseenter(function(){
+		$("#containerThreeTwitter").animate({width: '+=50px'}, "fast");		
+	});
+
+	$("#containerThreeTwitter").mouseleave(function(){
+		$("#containerThreeTwitter").animate({width: '-=50px'}, "fast");		
+	});
+
+	$("#containerThreeLinkedIn").mouseenter(function(){
+		$("#containerThreeLinkedIn").animate({width: '+=50px'}, "fast");		
+	});
+
+	$("#containerThreeLinkedIn").mouseleave(function(){
+		$("#containerThreeLinkedIn").animate({width: '-=50px'}, "fast");		
+	});
+
+	$("#containerThreeTextResume").mouseenter(function(){
+		$("#containerThreeTextResume").animate({fontSize: '+=1.5em'}, "fast");		
+	});
+
+	$("#containerThreeTextResume").mouseleave(function(){
+		$("#containerThreeTextResume").animate({fontSize: '-=1.5em'}, "fast");		
+	});
+
+	$("#containerThreeGmail").mouseenter(function(){
+		$("#containerThreeGmail").animate({width: '+=50px'}, "fast");		
+	});
+
+	$("#containerThreeGmail").mouseleave(function(){
+		$("#containerThreeGmail").animate({width: '-=50px'}, "fast");		
+	});
+});
